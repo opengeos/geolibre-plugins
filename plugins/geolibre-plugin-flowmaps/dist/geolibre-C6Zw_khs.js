372fd99145e04497379c47c03f24635992f04ce1
Robert Erlenbusch <kai.erlenbusch@pitt.edu>
2026-07-22 23:48:51 -0500

    fix: prevent maxTopFlowsDisplayNum from clamping to 0 for small datasets

---
+++ plugins/geolibre-plugin-flowmaps/dist/geolibre-C6Zw_khs.js
@@ -0,0 +1,44 @@
+const PLUGIN_DATA_PARAM = "plugin-data";
+function getPluginDataValue(params) {
+  var _a;
+  const trimmed = (_a = params.get(PLUGIN_DATA_PARAM)) == null ? void 0 : _a.trim();
+  return trimmed ? trimmed : null;
+}
+async function maybeHandleDeepLink(consumer, params) {
+  const value = getPluginDataValue(params);
+  if (value) await consumer.loadFromUrl(value);
+}
+let pluginInstance = null;
+const plugin = {
+  id: "geolibre-plugin-flowmaps",
+  name: "Flowmaps.gl",
+  version: "0.1.0",
+  urlParameterNames: [PLUGIN_DATA_PARAM],
+  async activate(app) {
+    if (!pluginInstance) {
+      const { FlowmapsPluginHost } = await import("./host-impl-Bfprh8gd.js");
+      pluginInstance = new FlowmapsPluginHost();
+    }
+    await pluginInstance.activate(app);
+  },
+  handleUrlParameters(app, params) {
+    if (pluginInstance) pluginInstance.handleUrlParameters(app, params);
+  },
+  deactivate(app) {
+    if (pluginInstance) {
+      pluginInstance.deactivate(app);
+    }
+  },
+  getProjectState() {
+    return (pluginInstance == null ? void 0 : pluginInstance.getProjectState()) ?? void 0;
+  },
+  applyProjectState(app, state) {
+    if (pluginInstance) {
+      pluginInstance.applyProjectState(app, state);
+    }
+  }
+};
+export {
+  maybeHandleDeepLink as m,
+  plugin as p
+};
--- plugins/geolibre-plugin-flowmaps/dist/index.js
+++ plugins/geolibre-plugin-flowmaps/dist/index.js
@@ -1,4 +1,4 @@
-import { p, p as p2 } from "./geolibre-CWkFnVQp.js";
+import { p, p as p2 } from "./geolibre-C6Zw_khs.js";
 export {
   p as default,
   p2 as plugin
